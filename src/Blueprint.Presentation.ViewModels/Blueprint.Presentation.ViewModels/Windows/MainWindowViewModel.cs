bd82291f641f7afda39d4f69e206cc1ea7862acb
Ulisses <ulisses.albino@remsoft.com>
2026-03-20 17:21:30 -0300

    Fix

--- src/Blueprint.Presentation.ViewModels/Blueprint.Presentation.ViewModels/Windows/MainWindowViewModel.cs
+++ src/Blueprint.Presentation.ViewModels/Blueprint.Presentation.ViewModels/Windows/MainWindowViewModel.cs
@@ -1,53 +1,55 @@
-﻿using Blueprint.Presentation.ViewModels.Core;
+﻿using System.Collections.ObjectModel;
+using Blueprint.Presentation.ViewModels.Core;
 using Wpf.Ui.Controls;
 
 namespace Blueprint.Presentation.ViewModels.Windows;
 
 public class MainWindowViewModel : NotifyPropertyChangedBase
 {
-    private readonly List<MenuItem> _trayMenuItems =
+    private readonly ObservableCollection<MenuItem> _trayMenuItems =
     [
         new MenuItem { Header = "Home", Tag = "tray_home" }
     ];
 
-    private readonly string _applicationTitle = "Bllueprint";
-    private readonly List<object> _footerMenuItems =
-    [
-        new NavigationViewItem
+    private readonly ObservableCollection<NavigationViewItem> _footerMenuItems = [];
+    private readonly ObservableCollection<NavigationViewItem> _menuItems = [];
+
+    public string ApplicationTitle { get; } = "Bllueprint";
+
+    public ObservableCollection<NavigationViewItem> MenuItems => _menuItems;
+
+    public ObservableCollection<NavigationViewItem> FooterMenuItems => _footerMenuItems;
+
+    public ObservableCollection<MenuItem> TrayMenuItems => _trayMenuItems;
+
+    public void Initialize(Type settingsPageType, Type dashboardPageType, Type dataPageType, Type codePageType)
+    {
+        _footerMenuItems.Add(new NavigationViewItem
         {
             Content = "Settings",
             Icon = new SymbolIcon { Symbol = SymbolRegular.Settings24 },
-            ////TargetPageType = typeof(Views.Pages.SettingsPage)
-        }
-    ];
-
-    private readonly List<object> _menuItems =
-    [
+            TargetPageType = settingsPageType
+        });
+        _menuItems.Add(
         new NavigationViewItem
         {
             Content = "Home",
             Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
-            ////TargetPageType = typeof(Views.Pages.DashboardPage)
-        },
+            TargetPageType = dashboardPageType
+        });
+        _menuItems.Add(
         new NavigationViewItem
         {
             Content = "Data",
             Icon = new SymbolIcon { Symbol = SymbolRegular.DataHistogram24 },
-            ////TargetPageType = typeof(Views.Pages.DataPage)
-        },
+            TargetPageType = dataPageType
+        });
+        _menuItems.Add(
         new NavigationViewItem
         {
             Content = "Code",
             Icon = new SymbolIcon { Symbol = SymbolRegular.Code16 },
-            ////TargetPageType = typeof(Views.Pages.CodePage)
-        }
-    ];
-
-    public string ApplicationTitle => _applicationTitle;
-
-    public IEnumerable<object> MenuItems => _menuItems;
-
-    public IEnumerable<object> FooterMenuItems => _footerMenuItems;
-
-    public IEnumerable<object> TrayMenuItems => _trayMenuItems;
+            TargetPageType = codePageType
+        });
+    }
 }
--- src/Blueprint/Views/Windows/MainWindow.xaml.cs
+++ src/Blueprint/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Wpf.Ui.Appearance;
+﻿using Blueprint.Presentation.ViewModels.Windows;
+using Blueprint.Views.Pages;
+using Wpf.Ui.Appearance;
 
 namespace Blueprint.Views.Windows;
 
@@ -7,6 +9,10 @@ public partial class MainWindow
     public MainWindow()
     {
         InitializeComponent();
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            viewModel.Initialize(typeof(SettingsPage), typeof(DashboardPage), typeof(DataPage), typeof(CodePage));
+        }
     }
 
     public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
@@ -14,7 +20,7 @@ public partial class MainWindow
     protected override void OnClosed(EventArgs e)
     {
         base.OnClosed(e);
-        Application.Current.Shutdown();
+        System.Windows.Application.Current.Shutdown();
     }
 
     protected override void OnInitialized(EventArgs e)
