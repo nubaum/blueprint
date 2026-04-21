965a9568f8513e1519fbd5a42a287e6a22167895
Nubaum <poorskills@outlook.com>
2026-04-21 19:01:34 -0300

    Adding api

---
+++ blueprint-backend/src/Blueprint.Api/Program.cs
@@ -0,0 +1,40 @@
+namespace Blueprint.Api;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddOpenApi();
+
+        ConfigureServices(builder.Services);
+
+        var app = builder.Build();
+
+        Configure(app);
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        await app.RunAsync();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        //// services.AddSwaggerGen();
+        services.AddControllers();
+    }
+
+    private static void Configure(WebApplication app)
+    {
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
